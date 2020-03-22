570232e04b20cb2bf2ef4a7c5f9f1a54fb11d597
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-03-22 22:28:28 +0900

    init

---
+++ algorithm/algorithm.h
@@ -0,0 +1,19 @@
+//
+//  algorithm.h
+//  algorithm
+//
+//  Created by 山本裕太 on 2020/03/22.
+//  Copyright © 2020 YutaYamamoto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for algorithm.
+FOUNDATION_EXPORT double algorithmVersionNumber;
+
+//! Project version string for algorithm.
+FOUNDATION_EXPORT const unsigned char algorithmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <algorithm/PublicHeader.h>
+
+
